6bbc038d92530d4eb7f86bf1258061be80c7dd59
Ernesto De Los Santos <netos_id@yahoo.com>
2016-06-03 22:29:32 -0500

    working without icons

---
+++ bundle.js
@@ -0,0 +1,70 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(process, __dirname) {'use strict';
+
+	var express = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"express\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	var app = express();
+
+	app.set('port', process.env.PORT || 5000);
+
+	app.use(express.static(__dirname + '/public'));
+
+	// views is directory for all template files
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+
+	app.get('/', function (request, response) {
+	  response.render('pages/index');
+	});
+
+	app.listen(app.get('port'), function () {
+	  console.log('Node app is running on port', app.get('port'));
+	});
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"/Users/ernestodelossantos/git/7.4-Chat-App/node_modules/process/browser.js\""); e.code = 'MODULE_NOT_FOUND'; throw e; }())), "/"))
+
+/***/ }
+/******/ ]);
